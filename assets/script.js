2f9bf745a7610bd9468abc40727d8874e9237845
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-24 20:38:18 -0300

    ajuste em js

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,105 @@
-/*
+const data = new Date()
+let h = data.getHours()
+let minutos = data.getMinutes()
+let segundos = data.getSeconds()
+
+
+
 function carregandoPagina() { 
-    let relogio = document.querySelector('#relogio')
-    let image = document.querySelector('#img')
-    let data = new Date()
-    let h = data.getHours()
+    let div = document.querySelector('#relogio')
+    let img = document.querySelector('#img')
 
 
-    if (h >= 6 && h <= 12 ) { 
-       relogio.innerHTML = 'Dia' 
+    let link = document.querySelector('#link')
+    let p = document.querySelector('#paragrafo')
+
+    if (h >= 1 && h <= 5) { 
+        div.innerHTML = 'madrugada' 
+        img.src = 'assets/img/madrugada.jpg'
+        document.body.style.backgroundColor = '#2D333B'
+        link.style.color = 'white'
+        p.style.color = 'white'
+
+    } else if (h >= 6 && h <= 12) {
+        div.innerHTML = `manha`
+        img.src = 'assets/img/manha.jpg'
 
     } else if (h >= 13 && h <= 18) {
-        relogio.innerHTML = `tarde`
+        div.innerHTML = `tarde`
+        img.src = 'assets/img/tarde.jpg'
+        document.body.style.backgroundColor = '#f2a663ef'
 
-    } else if (h >= 19 && h <= 0 ) { // não funcionar??
-        relogio.innerHTML = `Noite`
-
-    } else  if (h >= 1 && h <= 5) {
-        relogio.innerHTML = `madrugada`
+    } else {
+        div.innerHTML = `noite`
+        img.src = 'assets/img/noite.jpg'
+        document.body.style.backgroundColor = '#35302fc9'
+        link.style.color = 'white'
+        p.style.color = 'white'
     }
     
-} */
+}
+
+
+
+
+
+
+/*
+const data = new Date()
+let h = data.getHours()
+let minutos = data.getMinutes()
+let segundos = data.getSeconds()
+
+
 
 function carregandoPagina() { 
     let relogio = document.querySelector('#relogio')
     let img = document.querySelector('#img')
-    let data = new Date()
-    let h = data.getHours()
 
     if (h >= 1 && h <= 5) { 
-       relogio.innerHTML = 'madrugada' 
-       img.src = 'assets/img/madrugada.jpg'
+    relogio.innerHTML = 'madrugada' 
+    img.src = 'assets/img/madrugada.jpg'
 
     } else if (h >= 6 && h <= 12) {
-        relogio.innerHTML = `manha`
-        img.src = 'assets/img/manha.jpg'
+    relogio.innerHTML = `manha`
+    img.src = 'assets/img/manha.jpg'
 
     } else if (h >= 13 && h <= 18) {
-        relogio.innerHTML = `tarde`
-        img.src = 'assets/img/tarde.jpg'
+    relogio.innerHTML = `tarde`
+    img.src = 'assets/img/tarde.jpg'
     } else {
-        relogio.innerHTML = `noite`
-        img.src = 'assets/img/noite.jpg'
+    relogio.innerHTML = `noite`
+    img.src = 'assets/img/noite.jpg'
     }
     
 }
+*/
+
+
+
+// function carregandoPagina() { 
+//     let relogio = document.querySelector('#relogio')
+//     let img = document.querySelector('#img')
+//     let data = new Date()
+//     let h = 19 //data.getHours()
+
+
+//     if (h >= 6 && h <= 12 ) { 
+//        relogio.innerHTML = 'manha' 
+//         img.src = 'assets/img/manha.jpg'
+
+//     } else if (h >= 13 && h <= 18) {
+//         relogio.innerHTML = `tarde`
+//         img.src = 'assets/img/tarde.jpg'
+
+//     } else if (h >= 19 && h <= 0 ) { // não funcionar?
+//         relogio.innerHTML = `Noite`
+//         img.src = 'assets/img/noite.jpg'
+
+//     } else  if (h >= 1 && h <= 5) {
+//         relogio.innerHTML = `madrugada`
+//         img.src = 'assets/img/madrugada.jpg'
+//     }
+    
+// }
 
