8e4fcd976b7962f8f9527a4bb160752ba59ccc73
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-25 19:05:36 -0300

    relogio on

--- assets/script.js
+++ assets/script.js
@@ -58,29 +58,3 @@ function carregandoPagina() {
     
 }
 
-
-// function carregandoPagina() { 
-//     let relogio = document.querySelector('#relogio')
-//     let img = document.querySelector('#img')
-//     let data = new Date()
-//     let h = 19 //data.getHours()
-
-
-//     if (h >= 6 && h <= 12 ) { 
-//        relogio.innerHTML = 'manha' 
-//         img.src = 'assets/img/manha.jpg'
-
-//     } else if (h >= 13 && h <= 18) {
-//         relogio.innerHTML = `tarde`
-//         img.src = 'assets/img/tarde.jpg'
-
-//     } else if (h >= 19 && h <= 0 ) { // não funcionar?
-//         relogio.innerHTML = `Noite`
-//         img.src = 'assets/img/noite.jpg'
-
-//     } else  if (h >= 1 && h <= 5) {
-//         relogio.innerHTML = `madrugada`
-//         img.src = 'assets/img/madrugada.jpg'
-//     }
-    
-// }
